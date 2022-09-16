41053cc8acfbf98240de2ed4d496159a5ca83285
Luis Alfredo G. Caldas Neto <luisnt@gmail.com>
2022-09-16 13:53:27 -0300

    Solve bug port on cloud

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
-const httpsAgent = new require("https").Agent({ rejectUnauthorized: false });
-const axios = require("axios").create({ httpsAgent });
+const axios = require("axios").create({
+  httpsAgent: new require("https").Agent({ rejectUnauthorized: false }),
+});
 const express = require("express");
-var cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors());
+const port = 80;
 
 app.get("/", async (req, res) => {
   res.json({
     Api: "1.0",
     Engineer: "Luis Nt",
     Now: new Date(),
-    QueryUses: "/proxy?url=https://e-svt.herokuapp.com",
+    uses: {
+      head: "url: https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena",
+      query: "/proxy?url=https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena",
+      body: " { url : https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena } ",
+    },
   });
 });
 
 app.get("/proxy", async (req, res) => {
-  const { query } = req;
-  const url = query["url"];
+  const { headers: head, query, body } = req;
+  const url = head["url"] || query["url"] || body["url"];
+
   const { status, statusText, headers, data } = await axios.get(url);
-  res.json({ headers, status, statusText, data });
+  res.json({
+    head,
+    status,
+    statusText,
+    data,
+  });
 });
+
 
 const PORT = process.env.PORT || "8080";
 
 app.listen(PORT, () => {
   console.log('App proxy listening on port ', PORT);
 });
+
